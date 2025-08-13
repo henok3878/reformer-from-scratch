fdb18cfb8ba27b2aaefa0a8dfaf9c8dc72984a7e
henok3878 <henokwondimu3878@gmail.com>
2025-08-13 07:07:21 -0400

    feat: add TransformerRoPE

---
+++ src/reformer/transformer_rope.py
@@ -0,0 +1,18 @@
+from transformer.transformer import Transformer
+from transformer.config import ModelConfig, DataConfig
+from .components.multi_head_rope import MultiHeadAttentionRoPE
+
+class TransformerRoPE(Transformer):
+    """Transformer with RoPE - reuses all existing config parameters"""
+    
+    def __init__(self, model_config: ModelConfig, data_config: DataConfig):
+        super().__init__(
+            model_config=model_config,
+            data_config=data_config,
+            attention_cls=MultiHeadAttentionRoPE,
+            positional_encoding_cls=None,
+            use_input_positional_encoding=False,
+            src_max_len=model_config.src_max_len,
+            tgt_max_len=model_config.tgt_max_len,
+            base=10000.0 
+        )
